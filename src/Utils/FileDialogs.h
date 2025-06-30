395d8fbd92da842f19d3d8e1499f0791a7b99682
abdallhfattah <abdallh.fattah33@gmail.com>
2025-06-30 12:09:48 +0300

    refactor: move NormalizePath to FileDialogs utility class

--- src/Utils/FileDialogs.h
+++ src/Utils/FileDialogs.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <algorithm>
 #include <GLFW/glfw3.h>
 
 #ifdef _WIN32
@@ -15,5 +16,12 @@ namespace Utils {
         // These return empty strings if cancelled
         static std::string OpenFile(const char* title, const char* filter, GLFWwindow* window = nullptr);
         static std::string SaveFile(const char* title, const char* filter, GLFWwindow* window = nullptr);
+        // Utility function to normalize file paths
+        static std::string NormalizePath(const std::string& path) {
+            std::string normalized = path;
+            // Replace Windows-style backslashes with forward slashes
+            std::replace(normalized.begin(), normalized.end(), '\\', '/');
+            return normalized;
+        }
     };
 }
